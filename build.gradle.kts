b95fbb805e954cecf1408d314497755f5de3042e
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2024-08-18 15:47:05 +0300

    added dependencies : Navigation, ViewModel, gson, retrofit, glide, room and splashscreen.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,15 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
 }
