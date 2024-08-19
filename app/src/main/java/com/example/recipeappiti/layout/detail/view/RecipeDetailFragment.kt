bb3506c4b448907057a740d492a8ad3ddab37fbe
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2024-08-19 16:30:27 +0300

    Structure: added a MVVM architecture. Addition of Required Fragments and Activities. Implementation of Bottom Navigation and Drawer Layout.

---
+++ app/src/main/java/com/example/recipeappiti/layout/detail/view/RecipeDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.example.recipeappiti.layout.detail.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recipeappiti.R
+
+class RecipeDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
+    }
+
+
+}
