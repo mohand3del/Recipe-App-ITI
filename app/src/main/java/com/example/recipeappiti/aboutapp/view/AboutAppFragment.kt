c9183d82776cb54dc10ae03d7199c55076ccf1c8
GreenVenom77 <hosome2222@gmail.com>
2024-08-29 19:24:17 +0300

    Finished the About App screen

---
+++ app/src/main/java/com/example/recipeappiti/aboutapp/view/AboutAppFragment.kt
@@ -0,0 +1,18 @@
+package com.example.recipeappiti.aboutapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipeappiti.R
+
+class AboutAppFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_app, container, false)
+    }
+}
